09aab4578abb1dc8624414f4a106dda7c437f1b4
YounJoo Seog <reina.seog@gmail.com>
2022-05-05 00:16:38 +0900

    [front][Modify] Question5 페이지 구현

--- front/src/components/appbtitest/Pages/Question5.jsx
+++ front/src/components/appbtitest/Pages/Question5.jsx
@@ -1,31 +1,29 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import React from "react";
+import { useNavigate, useLocation } from "react-router";
 import QuestionLists from "../Components/QuestionLists";
-import AnswerLists from "../Components/AnswerLists";
 
-function Question1({ answers, setAnswers }) {
+function Question5() {
   const navigate = useNavigate();
-  const [testResult, setTestResult] = useState([]);
+  const { state } = useLocation();
   const i = 4;
 
-  const SaveAnswers = (e) => {
-    if (e.value === "a1") answers.push(AnswerLists[0].value);
-    else if (e.value === "a2") answers.push(AnswerLists[1].value);
-    console.log(answers);
-    // setAnswers((prev) => [...prev], answers);
-    navigate(`/AppbtiTest/6`);
+  const SavingAnswers = (e) => {
+    const answer = e;
+    const q5answer = state.concat(answer);
+    console.log("q5answer", q5answer);
+    navigate(`/AppbtiTest/6`, { state: q5answer });
   };
 
   return (
     <article className="area">
       <p>{QuestionLists[i].q1}</p>
-      <button id="a1" type="button" value="a1" onClick={(e) => SaveAnswers("a1", e.value)}>
+      <button id="a1" type="button" value="e1" onClick={() => SavingAnswers("e1")}>
         {QuestionLists[i].a1}
       </button>
-      <button id="a2" type="button" value="a2" onClick={(e) => SaveAnswers("a2", e.value)}>
+      <button id="a2" type="button" value="e2" onClick={() => SavingAnswers("e2")}>
         {QuestionLists[i].a2}
       </button>
     </article>
   );
 }
-export default Question1;
+export default Question5;
