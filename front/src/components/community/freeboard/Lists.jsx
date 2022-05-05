c3102a8d8bf13f39abc0804a88ff8fa3d15ac747
YounJoo Seog <reina.seog@gmail.com>
2022-05-05 16:10:19 +0900

    [front][style] 자유게시판 게시글 목록 css 수정

--- front/src/components/community/freeboard/Lists.jsx
+++ front/src/components/community/freeboard/Lists.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardContent, Typography } from "@mui/material";
 import { get } from "../../../api";
-import { useNavigate } from "react-router";
+import styled from "styled-components";
 
 const Lists = ({ setViewType }) => {
-  const navigate = useNavigate();
   const [isFetchCompleted, setIsFetchCompleted] = useState(false);
   const [lists, setLists] = useState([]);
 
@@ -23,22 +21,53 @@ const Lists = ({ setViewType }) => {
   }
 
   return (
-    <div id="FreeboardLists">
+    <ListContainer>
       {lists.map((freeboard) => (
-        <Card sx={{ minWidth: 275 }} key={freeboard._id} onClick={() => navigate(`freeboards/${freeboard._id}`)}>
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              {freeboard.name}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              {freeboard.title}
-            </Typography>
-            <Typography variant="body2">{freeboard.content}</Typography>
-          </CardContent>
-        </Card>
+        <ListCard key={freeboard._id} onClick={() => setViewType("View")}>
+          <BoardAuthor>
+            ✨ <b>{freeboard.name}</b> ✨님이 작성하신 글이예요!
+          </BoardAuthor>
+          <BoardTitle>{freeboard.title}</BoardTitle>
+          {freeboard.content}
+        </ListCard>
       ))}
-    </div>
+    </ListContainer>
   );
 };
 
 export default Lists;
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+`;
+
+const ListCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  // align-items: center;
+  width: 1200px;
+  height: 150px;
+  padding: 1vh 1vh 2vh 1vh;
+  margin: 20px 0 10px 0;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 3px 10px #e4e4e4;
+  word-break: keep-all;
+`;
+
+const BoardAuthor = styled.div`
+  color: #a9a9a9;
+  text-align: right;
+`;
+
+const BoardTitle = styled.div`
+  color: #484bcc;
+  font-size: 1.5em;
+  font-weight: 800;
+  padding: 0 0 5px 0;
+`;
