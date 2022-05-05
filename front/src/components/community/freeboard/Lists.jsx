2a4ccc87813ebc858d8eb1c5bdc89a8672fecd70
YounJoo Seog <reina.seog@gmail.com>
2022-05-05 22:27:31 +0900

    [front][style] Lists Styled Component화

--- front/src/components/community/freeboard/Lists.jsx
+++ front/src/components/community/freeboard/Lists.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { get } from "../../../api";
-import styled from "styled-components";
+import { ListContainer, ListCard, BoardAuthor, BoardTitle, WriteButton } from "./ListsStyles";
 
 const Lists = ({ setViewType }) => {
   const [isFetchCompleted, setIsFetchCompleted] = useState(false);
@@ -48,54 +48,3 @@ const Lists = ({ setViewType }) => {
 };
 
 export default Lists;
-
-const ListContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-`;
-
-const ListCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  // align-items: center;
-  width: 1200px;
-  height: 150px;
-  padding: 1vh 1vh 2vh 1vh;
-  margin: 20px 0 10px 0;
-  background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0 3px 10px #e4e4e4;
-  word-break: keep-all;
-`;
-
-const BoardAuthor = styled.div`
-  color: #a9a9a9;
-  text-align: right;
-`;
-
-const BoardTitle = styled.div`
-  color: #484bcc;
-  font-size: 1.5em;
-  font-weight: 800;
-  padding: 0 0 5px 0;
-`;
-const WriteButton = styled.div`
-  display: flex;
-  float: right;
-  justify-content: center;
-  align-items: center;
-  width: 120px;
-  height: 40px;
-  color: white;
-  background-color: #484bcc;
-  &:hover {
-    background-color: #5355c9;
-  }
-  border-radius: 8px;
-  box-shadow: 0 4px 6px #e4e4e4;
-  word-break: keep-all;
-`;
