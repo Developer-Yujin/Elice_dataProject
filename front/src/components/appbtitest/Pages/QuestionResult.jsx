44a5b36667a168b82f0d1e746e0eb73370129d07
YounJoo Seog <reina.seog@gmail.com>
2022-05-06 18:48:07 +0900

    [git][refactor] 결과 화면 수정중 ...

--- front/src/components/appbtitest/Pages/QuestionResult.jsx
+++ front/src/components/appbtitest/Pages/QuestionResult.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 import { get, post } from "../../../api";
 import styled from "styled-components";
@@ -7,16 +8,15 @@ function AppbtiTest() {
   const [isFetchCompleted, setIsFetchCompleted] = useState(false);
   const [finalAnswer, setFinalAnswer] = useState([]);
   const { state } = useLocation();
-  // const CompleteAnswer = state.join("");
 
   useEffect(() => {
     async function loadAnswerResult() {
-      console.log(state);
-      await post(`appbti`, {
+      const res = await post(`appbti`, {
         answers: state,
       });
-      const res = await get(`appbti`);
-      setFinalAnswer(res.data.result);
+      setFinalAnswer(res.data);
+      console.clear();
+      console.log(res.data);
       setIsFetchCompleted(true);
     }
     loadAnswerResult();
@@ -27,11 +27,23 @@ function AppbtiTest() {
     return "Loading...";
   }
 
+  const LinkAppstore = (id) => {
+    const appid = id.currentTarget;
+    window.open(`http://www.play.google.com/store/apps/details?id=${appid}`, "_blank");
+  };
+
   return (
     <AnswerCardContainer>
       <FinalAnswer>당신에게 추천 드리는 앱은!</FinalAnswer>
       {finalAnswer.map((fa) => (
-        <AnswerCard key={fa.index}>{fa}</AnswerCard>
+        <AnswerCard key={fa._id}>
+          <AppImgBox>이미지암거낭</AppImgBox>
+          <DescriptionBox>
+            <AppTitleBox>{fa.name}</AppTitleBox>
+            <AppCTGRBox>{fa.category}</AppCTGRBox>
+            <AppStarBox>{fa.rating}</AppStarBox>
+          </DescriptionBox>
+        </AnswerCard>
       ))}
     </AnswerCardContainer>
   );
@@ -60,15 +72,62 @@ const AnswerCardContainer = styled.div`
 const AnswerCard = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
-  width: 150px;
+  width: 250px;
   height: 150px;
-  padding: 2vh;
-  margin: 5vh;
+  padding: 1vh;
+  margin: 3vh;
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 0 3px 10px #e4e4e4;
   word-break: keep-all;
   text-align: center;
+`;
+
+const AppImgBox = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+  align-items: center;
+  background-color: red;
+`;
+
+const DescriptionBox = styled.div`
+  width: 130px;
+  height: 120px;
+  // background-color: grey;
+  margin: auto;
+`;
+
+const AppTitleBox = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  // background-color: blue;
+  margin: 10px 5px 5px 5px;
+  width: 120px;
+  height: 45px;
+  font-weight: 900;
+  vertical-align: middle;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AppCTGRBox = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  // background-color: green;
+  margin: 5px;
+  font-style: italic;
+  font-size: 0.8em;
+`;
+
+const AppStarBox = styled.div`
+  // background-color: black;
+  margin: 5px;
 `;
