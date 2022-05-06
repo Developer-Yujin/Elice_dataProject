ae674d9f9ee7f600e77155689192188b05017349
YounJoo Seog <reina.seog@gmail.com>
2022-05-06 23:41:44 +0900

    [front][style] 결과 페이지 수정

--- front/src/components/appbtitest/Pages/QuestionResult.jsx
+++ front/src/components/appbtitest/Pages/QuestionResult.jsx
@@ -24,26 +24,39 @@ function AppbtiTest() {
 
   // * * Skeleton Code 작성할 것
   if (!isFetchCompleted) {
-    return "Loading...";
+    return (
+      <center>
+        <img src="https://cdn.discordapp.com/attachments/964509729659445289/969298919764934666/aac7e5d8fc1fe7bdcebf8025d862a8687414ad4a.gif" alt="작업중" />
+        <span>"열 심 히 작 업 중 이 랍 니 다."</span>
+      </center>
+    );
   }
 
-  const LinkAppstore = (id) => {
-    const appid = id.currentTarget;
-    window.open(`http://www.play.google.com/store/apps/details?id=${appid}`, "_blank");
-  };
+  const Star = finalAnswer.rating * 20;
 
   return (
     <AnswerCardContainer>
       <FinalAnswer>당신에게 추천 드리는 앱은!</FinalAnswer>
       {finalAnswer.map((fa) => (
         <AnswerCard key={fa._id}>
-          <AppImgBox onClick={() => window.open(`https://play.google.com/store/apps/details?id=${fa.id}`, "_blank")}>
-            <img width="100px" src={`${fa.icon}`} alt="앱 아이콘" />
+          <AppImgBox>
+            <img width="100px" src={`${fa.icon}`} alt="App icon" />
           </AppImgBox>
           <DescriptionBox>
             <AppTitleBox>{fa.name}</AppTitleBox>
             <AppCTGRBox>{fa.category}</AppCTGRBox>
-            <AppStarBox>{fa.rating}</AppStarBox>
+            <AppRatingBox>
+              <AppStarBox Star={`${fa.rating}` * 22.65}>
+                <img src="https://blog.kakaocdn.net/dn/mm2bG/btrBrt0kx9i/Lra7wbqiY389V7MG6slbNk/img.png" alt="별점" />
+              </AppStarBox>
+            </AppRatingBox>
+            <FooterBox>
+              <RatingNumber>{fa.rating}</RatingNumber>
+              <GoAppstore onClick={() => window.open(`https://play.google.com/store/apps/details?id=${fa.id}`, "_blank")}>
+                Go Store
+                <img src="https://blog.kakaocdn.net/dn/NGH2u/btrBr6RaP1a/sNHwzqgWWVkYLAEx96lHxk/img.png" alt="appbti icon" />
+              </GoAppstore>
+            </FooterBox>
           </DescriptionBox>
         </AnswerCard>
       ))}
@@ -109,7 +122,7 @@ const AppTitleBox = styled.div`
   -webkit-box-orient: vertical;
   word-wrap: break-word;
   // background-color: blue;
-  margin: 10px 5px 5px 5px;
+  margin: 2px 5px 2px 5px;
   width: 120px;
   height: 45px;
   font-weight: 900;
@@ -123,12 +136,43 @@ const AppCTGRBox = styled.div`
   text-overflow: ellipsis;
   white-space: nowrap;
   // background-color: green;
-  margin: 5px;
+  margin: 2px 5px 2px 5px;
   font-style: italic;
   font-size: 0.8em;
 `;
 
-const AppStarBox = styled.div`
-  // background-color: black;
+const AppRatingBox = styled.div`
+  background-color: black;
   margin: 5px;
+  width: 120px;
+  height: 20px;
+`;
+
+const AppStarBox = styled.div`
+  display: flex;
+  height: 20px;
+  background-color: yellow;
+  width: ${(props) => props.Star}px;
+`;
+
+const FooterBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+const RatingNumber = styled.div`
+  margin: 0px 5px 0px 0px;
+  font-weight: 600;
+`;
+const GoAppstore = styled.button`
+  color: white;
+  font-weight: 500;
+  padding: 1px 4px 1px 4px;
+  background-color: #484bcc;
+  &:hover {
+    background-color: #5355c9;
+  }
+  border-radius: 5px;
+  border: none;
 `;
